9dc5fc73bf6c6523c52dc2eb90f3e5a2ccf75f38
Andreas Monitzer <andreas@monitzer.com>
2024-02-28 22:24:41 +0100

    Finished code.

--- src/image_future.rs
+++ src/image_future.rs
@@ -1,22 +1,33 @@
-#![allow(dead_code, unused_variables, unused_mut)]
-
 use wasm_bindgen::prelude::*;
 use web_sys::HtmlImageElement;
 
 use std::{
+    cell::RefCell,
     future::Future,
     pin::Pin,
+    rc::Rc,
     task::{Context, Poll},
 };
 
+#[allow(dead_code)]
+struct ImageData {
+    image: HtmlImageElement,
+    resolve: Closure<dyn FnMut()>,
+    reject: Closure<dyn FnMut(JsValue)>,
+}
+
 pub struct ImageLoader {
     url: String,
+    data: Option<ImageData>,
+    error: Rc<RefCell<Option<JsValue>>>,
 }
 
 impl ImageLoader {
     pub fn load(url: &str) -> Self {
         Self {
             url: url.to_owned(),
+            data: None,
+            error: Rc::default(),
         }
     }
 }
@@ -25,6 +36,38 @@ impl Future for ImageLoader {
     type Output = Result<HtmlImageElement, JsValue>;
 
     fn poll(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        if let Some(ImageData { image, .. }) = self.data.take() {
+            if image.complete() {
+                if let Some(err) = self.error.borrow_mut().take() {
+                    return std::task::Poll::Ready(Err(err));
+                } else {
+                    return std::task::Poll::Ready(Ok(image));
+                }
+            }
+        } else {
+            let waker = cx.waker().clone();
+            let resolve = Closure::once(move || {
+                waker.wake();
+            });
+            let waker = cx.waker().clone();
+            let error = Rc::downgrade(&self.error);
+            let reject = Closure::once(move |err: JsValue| {
+                if let Some(error) = error.upgrade() {
+                    *error.borrow_mut() = Some(err);
+                }
+                waker.wake();
+            });
+
+            let image = HtmlImageElement::new().unwrap();
+            image.set_onload(Some(resolve.as_ref().unchecked_ref()));
+            image.set_onerror(Some(reject.as_ref().unchecked_ref()));
+            image.set_src(&self.url);
+            self.data = Some(ImageData {
+                image,
+                resolve,
+                reject,
+            });
+        }
         std::task::Poll::Pending
     }
 }
